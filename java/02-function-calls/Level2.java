8fff1a3cc1ad5187e274dd7509ac34d3f743b16a
Žiga Maj Špiler <zmspiler@pm.me>
2025-04-29 09:58:26 +0200

    Add Java samples (#2)
    * Add c# examples
    
    * Remove gitignore
    
    * Add java samples

---
+++ java/02-function-calls/Level2.java
@@ -0,0 +1,9 @@
+public class Level2 {
+    public static void main(String[] args) {
+        greetUser("Alice");
+    }
+
+    static void greetUser(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+}
---
+++ java/03-loops-and-conditionals/Level3.java
@@ -0,0 +1,21 @@
+public class Level3 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Count: " + i);
+
+            if (i > 3) {
+                System.out.println("More than 3");
+            } else if (i == 3) {
+                System.out.println(3);
+            } else {
+                System.out.println("Less than 3");
+            }
+        }
+
+        if (java.time.LocalTime.now().getHour() < 12) {
+            System.out.println("Good morning!");
+        } else {
+            System.out.println("Good afternoon!");
+        }
+    }
+}
