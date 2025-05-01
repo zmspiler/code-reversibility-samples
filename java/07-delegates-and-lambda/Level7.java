8187d43676562bfc41ea8f38040ec7ec81d08f3c
zmspiler <maj@passways.io>
2025-05-01 13:00:00 +0200

    Remove java samples

--- java/07-delegates-and-lambda/Level7.java
+++
@@ -1,8 +0,0 @@
-import java.util.function.BiFunction;
-
-public class Level7 {
-    public static void main(String[] args) {
-        BiFunction<Integer, Integer, Integer> add = (a, b) -> a + b;
-        System.out.printf("Result: %d\n", add.apply(3, 4));
-    }
-}
