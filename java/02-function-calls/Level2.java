f1ea7b3183786b71c9417d08e52e2ff3da52bfc3
Žiga Maj Špiler <zmspiler@pm.me>
2025-05-05 17:33:00 +0200

    Remove java samples, rename C# files, remove self-rewriting code (#7)
    * Remove java samples
    
    * Rename c# files to match their levels
    
    * Remove self rewriting code example
    
    ---------
    
    Co-authored-by: zmspiler <maj@passways.io>

--- java/02-function-calls/Level2.java
+++
@@ -1,9 +0,0 @@
-public class Level2 {
-    public static void main(String[] args) {
-        greetUser("Alice");
-    }
-
-    static void greetUser(final String name) {
-        System.out.printf("Hello, %s!\n", name);
-    }
-}
